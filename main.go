9fcd601cf8fd84ed0dfec2ab0fc7794abc50997d
mohammed jebbari <mohammed.jebbari24@ump.ac.ma>
2024-10-13 16:54:55 +0100

    push

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	fmt.Println("Server is running on http://localhost:8080/")
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	http.HandleFunc("/", groupietracker.HomeHandler)
 	http.HandleFunc("/artists", groupietracker.ArtistsHandler)
 	err := http.ListenAndServe(":8080", nil)
